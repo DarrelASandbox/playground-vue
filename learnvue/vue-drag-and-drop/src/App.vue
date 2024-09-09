509c0ae27c93f6b2bd097b0ea18e99f902c9b5c4
DarrelA <darrelaiscoding@gmail.com>
2024-09-09 12:13:04 +0800

    `items` & `getList`

--- learnvue/vue-drag-and-drop/src/App.vue
+++ learnvue/vue-drag-and-drop/src/App.vue
@@ -1,7 +1,45 @@
-<script></script>
+<script setup>
+import { ref } from 'vue';
+
+const items = ref([
+  { id: 0, title: 'Item A', list: 1 },
+  { id: 1, title: 'Item B', list: 1 },
+  { id: 2, title: 'Item C', list: 2 },
+]);
+
+const getList = (list) => items.value.filter((item) => item.list === list);
+</script>
 
 <template>
-  <div>Vue Drag and Drop</div>
+  <div class="drop-zone">
+    <div v-for="item in getList(1)" :key="item.id" class="drag-el">
+      {{ item.title }}
+    </div>
+  </div>
+  <div class="drop-zone">
+    <div v-for="item in getList(2)" :key="item.id" class="drag-el">
+      {{ item.title }}
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style>
+.drop-zone {
+  width: 50%;
+  margin: 50px auto;
+  background-color: #ecf0f1;
+  padding: 10px;
+  min-height: 10px;
+}
+
+.drag-el {
+  background-color: #3498db;
+  color: white;
+  padding: 5px;
+  margin-bottom: 10px;
+}
+
+.drag-el:nth-last-of-type(1) {
+  margin-bottom: 0;
+}
+</style>
