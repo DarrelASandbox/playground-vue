d6ff4cb370651281ae39761798fc7f96cc3efa0f
DarrelA <darrelaiscoding@gmail.com>
2024-09-11 12:49:19 +0800

    `TaskDetails`: Accessing store state

--- net-ninja/pinia-tasks/src/App.vue
+++ net-ninja/pinia-tasks/src/App.vue
@@ -1,10 +1,21 @@
-<script setup></script>
+<script setup>
+import TaskDetails from './components/TaskDetails.vue';
+import { useTaskStore } from './stores/TaskStore';
+
+const taskStore = useTaskStore();
+</script>
 
 <template>
   <main>
     <header>
       <img src="./assets/pinia-logo.svg" alt="pinia logo" />
-      <h1>Pinia Tasks</h1>
+      <h1>Pinia Task</h1>
     </header>
+
+    <div class="task-list">
+      <div v-for="task in taskStore.tasks" :key="task.id">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
   </main>
 </template>
