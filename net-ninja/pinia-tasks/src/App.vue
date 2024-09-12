bf1b5562859daf15b57361699d0f7ea685dee85b
DarrelA <darrelaiscoding@gmail.com>
2024-09-12 13:38:53 +0800

    `getters`: `favTasks`, `favTaskcount` & `totalTaskCount`

--- net-ninja/pinia-tasks/src/App.vue
+++ net-ninja/pinia-tasks/src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
+import { ref } from 'vue';
 import TaskDetails from './components/TaskDetails.vue';
 import { useTaskStore } from './stores/TaskStore';
 
 const taskStore = useTaskStore();
+const showTasks = ref('all');
 </script>
 
 <template>
@@ -12,8 +14,21 @@ const taskStore = useTaskStore();
       <h1>Pinia Task</h1>
     </header>
 
-    <div class="task-list">
+    <nav class="filter">
+      <button @click="showTasks = 'all'">All Tasks</button>
+      <button @click="showTasks = 'fav'">Fav Tasks</button>
+    </nav>
+
+    <div class="task-list" v-if="showTasks === 'all'">
+      <p>Total Tasks: {{ taskStore.totalTaskCount }}</p>
       <div v-for="task in taskStore.tasks" :key="task.id">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
+
+    <div class="task-list" v-if="showTasks === 'fav'">
+      <p>Fav Tasks: {{ taskStore.favTaskCount }}</p>
+      <div v-for="task in taskStore.favTasks" :key="task.id">
         <TaskDetails :task="task" />
       </div>
     </div>
--- net-ninja/pinia-tasks/src/stores/TaskStore.js
+++ net-ninja/pinia-tasks/src/stores/TaskStore.js
@@ -7,4 +7,10 @@ export const useTaskStore = defineStore('taskStore', {
       { id: 2, title: 'play Gloomhaven', isFav: true },
     ],
   }),
+
+  getters: {
+    favTasks: (state) => state.tasks.filter((task) => task.isFav),
+    favTaskCount: (state) => state.tasks.reduce((p, c) => (c.isFav ? p + 1 : p), 0),
+    totalTaskCount: (state) => state.tasks.length,
+  },
 });
